0e2b6bcc233fef01dbd60035ce5aa31d4c129437
tommy-tommy-tom <105212576+tom-mcivor@users.noreply.github.com>
2023-02-12 00:24:29 +1300

    add

--- build/js/main.js
+++ build/js/main.js
@@ -1,21 +1,54 @@
 "use strict";
-class Coder {
-    constructor(name, music, age, lang = 'Typescript') {
-        this.name = name;
-        this.music = music;
-        this.age = age;
-        this.lang = lang;
-        this.name = name;
-        this.music = music;
-        this.age = age;
-        this.age = age;
-        this.lang = lang;
+const echo = (arg) => arg;
+const isObj = (arg) => {
+    return (typeof arg === 'object' && !Array.isArray)(arg) && arg !== null;
+};
+console.log(isfalse);
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+const isTrue = (arg) => {
+    if (Array.isArray(arg) && !arg.length) {
+        return { arg, is: !!arg };
     }
-    getAge() {
-        return `Hello, I'm ${this.age}`;
+    if (isObj(arg) && !Object.keys(arg).length) {
+        return { value: arg, is: false };
+    }
+    return { value: arg, is: !!arg };
+};
+console.log(isTrue(false));
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+const processUser = (user) => {
+    // process the user with logic here
+    return user;
+};
+console.log(processUser);
+const getUsersProperty = (users, key) => {
+    return users.map(user => user[key]);
+};
+const usersArray = [
+    {
+        "id": 1,
+        "name": "Lea"
+    }
+];
+console.log(getUsersProperty(usersArray, "email"));
+console.log(getUsersProperty());
+class StateObject {
+    constructor(value) {
+        this.data = value;
     }
 }
-const Dave = new Coder('Dave', 'Rock', 42);
-console.log(Dave.getAge());
-console.log(Dave.age);
-console.log(Dave.lang);
+const store = new StateObject("John");
+console.log(store.state);
+store.state = "Dave";
+// store.state = 12
+const myState = new StateObject([15]);
+myState.state = (['Dave']);
--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,98 @@
-class Coder {
+const echo =  <T>(arg: T): T => arg 
 
-  secondLang!: string
+const isObj = <T>(arg: T): boolean => {
+  return (typeof arg === 'object' && !Array.isArray)
+  (arg) && arg !== null
+}
 
-  constructor(
-    public readonly name: string,
-    public music: string,
-    private age: number,
-    protected lang: string = 'Typescript'
-    ){
-    this.name = name
-    this.music = music 
-    this.age = age
-    this.age = age 
-    this.lang = lang
+console.log(isfalse);
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+
+interface BoolCheck<T> {
+ value: T,
+ is: boolean,
+}
+
+const isTrue = <T>(arg: T): { arg: T, is: boolean} => {
+  if (Array.isArray(arg) && !arg.length) {
+    return { arg, is: !!arg}
+    
   }
 
-  public getAge(){
-    return `Hello, I'm ${this.age}`
+  if (isObj(arg) && !Object.keys(arg as keyof T).length) {
+    return { value: arg, is: false}
+  }
+  return { value: arg, is: !!arg }
+}
+
+
+
+console.log(isTrue(false));
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
+
+interface HasID {
+  id : number
+}
+
+const processUser = <T extends HasID>(user: T): T => {
+
+  // process the user with logic here
+  return user
+}
+
+
+
+console.log(processUser);
+
+const getUsersProperty = <T extends HasID, K extends keyof T>(users: T[], key: K): T[K][] => {
+  return users.map(user => user[key])
+}
+
+
+const usersArray = [
+ {
+  "id": 1,
+"name": "Lea"
+ }
+]
+
+console.log(getUsersProperty(usersArray, "email"))
+
+console.log(getUsersProperty());
+
+class StateObject<T> {
+  private data: T
+  constructor(value: T) {
+    this.data = value
   }
 }
 
-const Dave = new Coder('Dave', 'Rock', 42)
-console.log(Dave.getAge())
-console.log(Dave.age)
-console.log(Dave.lang)
+const store = new StateObject("John")
+console.log(store.state)
+store.state = "Dave"
+// store.state = 12
+
+const myState = new StateObject<(string|number|boolean)[]>([15])
+myState.state = (['Dave'])
+
+
+
+
+
+
+
+
+
+
+
+
 
